1e61bb3f0264a334aaac596745a33568a68a37e7
Yiffyi Jia <daniel_sy@live.com>
2024-07-07 19:57:12 +0800

    add: detail error message and time

--- watchdog.go
+++ watchdog.go
@@ -2,6 +2,7 @@ package waterplz
 
 import (
 	"errors"
+	"fmt"
 	"log/slog"
 	"strings"
 	"time"
@@ -18,22 +19,25 @@ type Watchdog struct {
 	LoopInterval float64
 	DeviceMac    string
 
-	sess  *upstream.Session
-	wecom *notification.WeComBot
-	tz    *time.Location
-	down  bool
+	sess       *upstream.Session
+	wecom      *notification.WeComBot
+	tz         *time.Location
+	down       bool
+	lastOnline time.Time
 }
 
 func (w *Watchdog) notifyError(err error) {
 	md := "# 热水系统 Down\n"
-	md += "当前时间：" + time.Now().In(w.tz).String() + "\n"
+	md += "当前时间：" + time.Now().In(w.tz).Format("2006-01-02 15:04:05 -0700 MST") + "\n"
 	md += `故障原因：<font color="warning">` + err.Error() + `</font>`
 	w.wecom.SendMarkdown(md)
 }
 
 func (w *Watchdog) notifyRecover() {
+	now := time.Now().In(w.tz)
 	md := "# 热水系统 Up\n"
-	md += "时间：" + time.Now().In(w.tz).String() + "\n"
+	md += "当前时间：" + now.Format("2006-01-02 15:04:05 -0700 MST") + "\n"
+	md += fmt.Sprintf(`故障持续时间：<font color="warning">%.1f 分钟</font>`, now.Sub(w.lastOnline).Minutes())
 	w.wecom.SendMarkdown(md)
 }
 
@@ -67,11 +71,16 @@ func (w *Watchdog) checkDeviceStatus() error {
 		}
 
 		ol, ok := dev["isOnline"].(float64)
-		if ok && int(ol) == 1 {
-			slog.Info("checkDeviceStatus: device is online", "mac", m)
+		if !ok {
+			slog.Error("checkDeviceStatus: device not found", "mac", m, "ol", ol)
+			return errors.New("上游 DeviceInfoList 设备不存在")
 		} else {
-			slog.Error("checkDeviceStatus: device not found or offline", "mac", m, "ol", ol)
-			return errors.New("上游 DeviceInfoList 设备不存在或离线")
+			if int(ol) == 1 {
+				slog.Info("checkDeviceStatus: device is online", "mac", m)
+			} else {
+				slog.Error("checkDeviceStatus: device offline", "mac", m, "ol", ol)
+				return errors.New("上游 DeviceInfoList 设备离线")
+			}
 		}
 	}
 
@@ -107,7 +116,7 @@ func (w *Watchdog) Start(bot *notification.WeComBot) {
 
 	ticker := time.NewTicker(time.Duration(w.LoopInterval) * time.Second)
 	for {
-		<-ticker.C
+		now := <-ticker.C
 
 		// 1. test connectivity by query money per order
 		// if !w.checkPerMoney() {
@@ -129,5 +138,6 @@ func (w *Watchdog) Start(bot *notification.WeComBot) {
 			w.down = false
 			w.notifyRecover()
 		}
+		w.lastOnline = now
 	}
 }
